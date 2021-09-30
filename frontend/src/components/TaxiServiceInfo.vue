1bdd2fe603e2df3c186fca69c07e5758da394384
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:26:23 +0900

    Create TaxiServiceInfo.vue

---
+++ frontend/src/components/TaxiServiceInfo.vue
@@ -0,0 +1,60 @@
+<template>
+
+<v-data-table
+    :headers="headers"
+    :items="taxiServiceInfo"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+
+</template>
+
+<script>
+  const axios = require('axios').default;
+
+  export default {
+    name: 'TaxiServiceInfo',
+    props: {
+      value: Object,
+      editMode: Boolean,
+      isNew: Boolean
+    },
+    data: () => ({
+        headers: [
+            { text: "callId", value: "callId" },
+            { text: "memberId", value: "memberId" },
+            { text: "taxiNumber", value: "taxiNumber" },
+            { text: "taxiInfo", value: "taxiInfo" },
+            { text: "memberName", value: "memberName" },
+            { text: "startAddress", value: "startAddress" },
+            { text: "endAddress", value: "endAddress" },
+            { text: "phoneNumber", value: "phoneNumber" },
+            { text: "friendPhoneNumber", value: "friendPhoneNumber" },
+            { text: "memberId", value: "memberId" },
+            { text: "taxiNumber", value: "taxiNumber" },
+            { text: "taxiInfo", value: "taxiInfo" },
+            { text: "memberName", value: "memberName" },
+            { text: "startAddress", value: "startAddress" },
+            { text: "endAddress", value: "endAddress" },
+            { text: "phoneNumber", value: "phoneNumber" },
+            { text: "friendPhoneNumber", value: "friendPhoneNumber" },
+            { text: "taxiStatus", value: "taxiStatus" },
+            { text: "taxiStatus", value: "taxiStatus" },
+            { text: "creditCardNumber", value: "creditCardNumber" },
+            { text: "payAmt", value: "payAmt" },
+            { text: "creditCardNumber", value: "creditCardNumber" },
+            { text: "payAmt", value: "payAmt" },
+        ],
+        taxiServiceInfo : [],
+    }),
+    async created() {
+      var temp = await axios.get(axios.backend + '/taxiserviceinfos')
+
+      this.taxiServiceInfo = temp.data._embedded.taxiserviceinfos;
+
+    },
+    methods: {
+    }
+  }
+</script>
+
