7d94a5805ee73a350b1ce032ab38948ba0c30fcd
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:31:06 +0900

    Create Payment.java

---
+++ PAYMENT/src/main/java/call/taxi/service/Payment.java
@@ -0,0 +1,74 @@
+package call.taxi.service;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+
+
+@Entity
+@Table(name="Payment_table")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long paymentId;
+    private Integer callId;
+    private String payDate;
+    private Long payAmt;
+    private String phoneNumber;
+    private String payStatus;
+
+    @PostPersist
+    public void onPostPersist(){
+        PaymentApproved paymentApproved = new PaymentApproved();
+        BeanUtils.copyProperties(this, paymentApproved);
+        paymentApproved.publishAfterCommit();
+
+    }
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+    public Integer getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Integer callId) {
+        this.callId = callId;
+    }
+    public String getPayDate() {
+        return payDate;
+    }
+
+    public void setPayDate(String payDate) {
+        this.payDate = payDate;
+    }
+    public Long getPayAmt() {
+        return payAmt;
+    }
+
+    public void setPayAmt(Long payAmt) {
+        this.payAmt = payAmt;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(String payStatus) {
+        this.payStatus = payStatus;
+    }
+
+
+
+
+}
