69c34b2f347a98107128aae1572aa60c42ceed1a
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:28:19 +0900

    Create MemberShipRegistered.java

---
+++ MEMBERSHIP/src/main/java/call/taxi/service/MemberShipRegistered.java
@@ -0,0 +1,58 @@
+package call.taxi.service;
+
+public class MemberShipRegistered extends AbstractEvent {
+
+    private Long memberId;
+    private String memberName;
+    private String phoneNumber;
+    private String address;
+    private String creditCardNumber;
+    private String friendPhoneNumber;
+
+    public MemberShipRegistered(){
+        super();
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+    public String getFriendPhoneNumber() {
+        return friendPhoneNumber;
+    }
+
+    public void setFriendPhoneNumber(String friendPhoneNumber) {
+        this.friendPhoneNumber = friendPhoneNumber;
+    }
+}
