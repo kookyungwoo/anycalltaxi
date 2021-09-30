44ba542ff0bb5770c166fd4e994b138bb72046d2
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:24:38 +0900

    Create TaxiCalled.java

---
+++ CALL/src/main/java/call/taxi/service/TaxiCalled.java
@@ -0,0 +1,42 @@
+package call.taxi.service;
+
+public class TaxiCalled extends AbstractEvent {
+
+    private Long callId;
+    private String memberId;
+    private String startAddress;
+    private String endAddress;
+
+    public TaxiCalled(){
+        super();
+    }
+
+    public Long getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Long callId) {
+        this.callId = callId;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+    public String getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(String startAddress) {
+        this.startAddress = startAddress;
+    }
+    public String getEndAddress() {
+        return endAddress;
+    }
+
+    public void setEndAddress(String endAddress) {
+        this.endAddress = endAddress;
+    }
+}
