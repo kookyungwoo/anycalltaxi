2964d61f6324ad62c49fa538fc28c5d6a68870e2
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:32:54 +0900

    Create Payment.java

---
+++ TAXI/src/main/java/call/taxi/service/external/Payment.java
@@ -0,0 +1,49 @@
+package call.taxi.service.external;
+
+public class Payment {
+
+    private Long paymentId;
+    private Integer callId;
+    private String payDate;
+    private Long payAmt;
+    private String phoneNumber;
+    private String payStatus;
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+    public Integer getCallId() {
+        return callId;
+    }
+    public void setCallId(Integer callId) {
+        this.callId = callId;
+    }
+    public String getPayDate() {
+        return payDate;
+    }
+    public void setPayDate(String payDate) {
+        this.payDate = payDate;
+    }
+    public Long getPayAmt() {
+        return payAmt;
+    }
+    public void setPayAmt(Long payAmt) {
+        this.payAmt = payAmt;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPayStatus() {
+        return payStatus;
+    }
+    public void setPayStatus(String payStatus) {
+        this.payStatus = payStatus;
+    }
+
+}
