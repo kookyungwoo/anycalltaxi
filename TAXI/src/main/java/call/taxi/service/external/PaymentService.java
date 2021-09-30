a221fb43e74989882ecb250dd32ec541feb8553a
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:33:06 +0900

    Create PaymentService.java

---
+++ TAXI/src/main/java/call/taxi/service/external/PaymentService.java
@@ -0,0 +1,15 @@
+package call.taxi.service.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@FeignClient(name="PAYMENT", url="http://PAYMENT:8080")
+public interface PaymentService {
+    @RequestMapping(method= RequestMethod.GET, path="/payments")
+    public void pay(@RequestBody Payment payment);
+
+}
+
