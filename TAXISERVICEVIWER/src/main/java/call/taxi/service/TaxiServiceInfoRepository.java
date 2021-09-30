c3ce34c4ccb4147fe9d9065e0919db9a7dc1e692
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:34:43 +0900

    Create TaxiServiceInfoRepository.java

---
+++ TAXISERVICEVIWER/src/main/java/call/taxi/service/TaxiServiceInfoRepository.java
@@ -0,0 +1,9 @@
+package call.taxi.service;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface TaxiServiceInfoRepository extends CrudRepository<TaxiServiceInfo, Long> {
+
+
+}
