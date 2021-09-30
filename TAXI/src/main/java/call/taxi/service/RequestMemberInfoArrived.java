eb2be6ae8bcde872046b9eb3f050538e4d81805d
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:33:13 +0900

    Create RequestMemberInfoArrived.java

---
+++ TAXI/src/main/java/call/taxi/service/RequestMemberInfoArrived.java
@@ -0,0 +1,46 @@
+package call.taxi.service;
+
+public class RequestMemberInfoArrived extends AbstractEvent {
+
+    private Long memberId;
+    private String memberName;
+    private String phoneNumber;
+    private String creditCardNumber;
+    private String friendPhoneNumber;
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+    public String getFriendPhoneNumber() {
+        return friendPhoneNumber;
+    }
+
+    public void setFriendPhoneNumber(String friendPhoneNumber) {
+        this.friendPhoneNumber = friendPhoneNumber;
+    }
+}
