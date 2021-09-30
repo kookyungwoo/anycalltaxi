61ea6ec1077632b6253e1b1e14ca3f5feb6d8aad
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:31:27 +0900

    Create PolicyHandler.java

---
+++ PAYMENT/src/main/java/call/taxi/service/PolicyHandler.java
@@ -0,0 +1,19 @@
+package call.taxi.service;
+
+import call.taxi.service.config.kafka.KafkaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @Autowired PaymentRepository paymentRepository;
+
+
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString){}
+
+
+}
