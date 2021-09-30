5a48a4940fac1d1e6eeca8df220348b4a621a6a3
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:33:43 +0900

    Create TaxiRepository.java

---
+++ TAXI/src/main/java/call/taxi/service/TaxiRepository.java
@@ -0,0 +1,10 @@
+package call.taxi.service;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel="taxis", path="taxis")
+public interface TaxiRepository extends PagingAndSortingRepository<Taxi, Long>{
+
+
+}
