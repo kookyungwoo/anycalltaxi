b1046d260fb596c5daf08932ab37e08e834a770f
kkw9437 <kkw9437@gmail.com>
2021-09-30 23:53:55 +0900

    Update PolicyHandler.java

--- TAXI/src/main/java/call/taxi/service/PolicyHandler.java
+++ TAXI/src/main/java/call/taxi/service/PolicyHandler.java
@@ -14,72 +14,71 @@ public class PolicyHandler{
     public void wheneverTaxiCalled_TaxiCall(@Payload TaxiCalled taxiCalled)
     {
         System.out.println(this.getClass().getName()+" : wheneverTaxiCalled_TaxiCall   -----------------start------------------");
-
-        //if(taxiCalled.isMe)
-        //{
             if(!taxiCalled.validate()) return;
     
             System.out.println("\n\n##### listener TaxiCall : " + taxiCalled.toJson() + "\n\n");
 
-
-
-            // Sample Logic //
-            // Taxi taxi = new Taxi();
-            // taxiRepository.save(taxi);
-        //}
+             Taxi taxi = new Taxi();
+             taxi.setCallId(taxiCalled.getCallId());
+             taxi.setMemberId(taxiCalled.getMemberId());
+             taxi.setStartAddress(taxiCalled.getStartAddress());
+             taxi.setEndAddress(taxiCalled.getEndAddress());
+             taxi.setCallStatus("택시호출");
+             taxiRepository.save(taxi);
         System.out.println(this.getClass().getName()+" : wheneverTaxiCalled_TaxiCall   -----------------end------------------");
     }
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverTaxiCallCanceled_CallCancel(@Payload TaxiCallCanceled taxiCallCanceled){
 
         System.out.println(this.getClass().getName()+" : wheneverTaxiCallCanceled_CallCancel   -----------------start------------------");
-        //if(taxiCallCanceled.isMe())
-        //{
+            
             if(!taxiCallCanceled.validate()) return;
 
             System.out.println("\n\n##### listener CallCancel : " + taxiCallCanceled.toJson() + "\n\n");
 
+            //taxiRepository.findById(taxiCallCanceled.getCallId()).ifPresent(Taxi->{taxiRepository.save(Taxi);});
+            
+            java.util.Optional<Taxi> optionalOrder = taxiRepository.findById(taxiCallCanceled.getCallId());
+            Taxi taxi = optionalOrder.get();
+            //order.setDeliveryId(String.valueOf(deleveryStarted.getId()));
+            taxi.setCallStatus("택시호출취소");
+            taxiRepository.save(taxi);
 
-
-            // Sample Logic //
-            // Taxi taxi = new Taxi();
-            // taxiRepository.save(taxi);
-        //}
         System.out.println(this.getClass().getName()+" : wheneverTaxiCallCanceled_CallCancel   -----------------end------------------");
     }
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverServiceEstimated_ServiceEstimate(@Payload ServiceEstimated serviceEstimated){
         System.out.println(this.getClass().getName()+" : wheneverServiceEstimated_ServiceEstimate   -----------------start------------------");
-        //if(serviceEstimated.isMe())
-        //{
+
             if(!serviceEstimated.validate()) return;
 
             System.out.println("\n\n##### listener ServiceEstimate : " + serviceEstimated.toJson() + "\n\n");
 
+            java.util.Optional<Taxi> optionalOrder = taxiRepository.findById(serviceEstimated.getCallId());
+            Taxi taxi = optionalOrder.get();
+            taxi.setEstimateScore(serviceEstimated.getEstimateScore());
+            taxiRepository.save(taxi);
 
 
-            // Sample Logic //
-            // Taxi taxi = new Taxi();
-            // taxiRepository.save(taxi);
-        //}
         System.out.println(this.getClass().getName()+" : wheneverServiceEstimated_ServiceEstimate   -----------------end------------------");
     }
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverRequestMemberInfoArrived_AddMemberInfo(@Payload RequestMemberInfoArrived requestMemberInfoArrived){
         System.out.println(this.getClass().getName()+" : wheneverRequestMemberInfoArrived_AddMemberInfo   -----------------start------------------");
 
-        //if(requestMemberInfoArrived.isMe())
-        //{
             if(!requestMemberInfoArrived.validate()) return;
 
             System.out.println("\n\n##### listener AddMemberInfo : " + requestMemberInfoArrived.toJson() + "\n\n");
 
+            java.util.Optional<Taxi> optionalOrder = taxiRepository.findById(requestMemberInfoArrived.getMemberId());
+            Taxi taxi = optionalOrder.get();
+            taxi.setMemberName(requestMemberInfoArrived.getMemberName());
+            taxi.setPhoneNumber(requestMemberInfoArrived.getPhoneNumber());
+            taxi.setCreditCardNumber(requestMemberInfoArrived.getCreditCardNumber());
+            taxi.setFriendPhoneNumber(requestMemberInfoArrived.getFriendPhoneNumber());
+            taxiRepository.save(taxi);
 
 
-            // Sample Logic //
-            // Taxi taxi = new Taxi();
-            // taxiRepository.save(taxi);
-       // }
         System.out.println(this.getClass().getName()+" : wheneverRequestMemberInfoArrived_AddMemberInfo   -----------------end------------------");
     }
 
