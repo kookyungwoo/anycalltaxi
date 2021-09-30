eaeceb32368846b1a0e670b295bae97a9884cf36
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:24:35 +0900

    Create TaxiCallCanceled.java

---
+++ CALL/src/main/java/call/taxi/service/TaxiCallCanceled.java
@@ -0,0 +1,42 @@
+package call.taxi.service;
+
+public class TaxiCallCanceled extends AbstractEvent {
+
+    private Long id;
+    private Long callId;
+    private String memberId;
+    private String phoneNumber;
+
+    public TaxiCallCanceled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Long callId) {
+        this.callId = callId;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
