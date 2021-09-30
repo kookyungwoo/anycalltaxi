8ec48c0d9adeef7b11e12fef609b42e833d25781
kkw9437 <kkw9437@gmail.com>
2021-09-30 23:53:45 +0900

    Update Payment.java

--- TAXI/src/main/java/call/taxi/service/external/Payment.java
+++ TAXI/src/main/java/call/taxi/service/external/Payment.java
@@ -8,7 +8,14 @@ public class Payment {
     private Long payAmt;
     private String phoneNumber;
     private String payStatus;
+    private String creditCardNumber;
 
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
     public Long getPaymentId() {
         return paymentId;
     }
