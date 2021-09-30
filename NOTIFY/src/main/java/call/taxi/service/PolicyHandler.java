f5d1cd1b0bb9924bbded4f445702e9a7c4f7f422
kkw9437 <kkw9437@gmail.com>
2021-09-30 23:53:30 +0900

    Update PolicyHandler.java

--- NOTIFY/src/main/java/call/taxi/service/PolicyHandler.java
+++ NOTIFY/src/main/java/call/taxi/service/PolicyHandler.java
@@ -17,7 +17,11 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener TaxiAssign : " + taxiAssigned.toJson() + "\n\n");
 
-
+        Notify notify = new Notify();
+        notify.setToPhoneNumber(taxiAssigned.getPhoneNumber());
+        notify.setFromPhoneNumber("02-1234-5678");
+        notify.setMessage("택시가 배정되었습니다 : "+taxiAssigned.getTaxiInfo());
+        notifyRepository.save(notify);
 
         // Sample Logic //
         // Notify notify = new Notify();
@@ -31,7 +35,11 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener ServiceStart : " + taxiServiceStarted.toJson() + "\n\n");
 
-
+        Notify notify = new Notify();
+        notify.setToPhoneNumber(taxiServiceStarted.getPhoneNumber());
+        notify.setFromPhoneNumber("02-1234-5678");
+        notify.setMessage("택시운행이 시작되었습니다 : "+taxiServiceStarted.getTaxiInfo());
+        notifyRepository.save(notify);
 
         // Sample Logic //
         // Notify notify = new Notify();
@@ -45,7 +53,11 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener ServiceEnd : " + taxiServiceEnded.toJson() + "\n\n");
 
-
+        Notify notify = new Notify();
+        notify.setToPhoneNumber(taxiServiceEnded.getPhoneNumber());
+        notify.setFromPhoneNumber("02-1234-5678");
+        notify.setMessage("택시운행이 종료되었습니다 : "+taxiServiceEnded.getTaxiInfo());
+        notifyRepository.save(notify);
 
         // Sample Logic //
         // Notify notify = new Notify();
@@ -59,7 +71,11 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener PayApprove : " + paymentApproved.toJson() + "\n\n");
 
-
+        Notify notify = new Notify();
+        notify.setToPhoneNumber(paymentApproved.getPhoneNumber());
+        notify.setFromPhoneNumber("02-1234-5678");
+        notify.setMessage("택시비용 결제요청 되었습니다");
+        notifyRepository.save(notify);
 
         // Sample Logic //
         // Notify notify = new Notify();
