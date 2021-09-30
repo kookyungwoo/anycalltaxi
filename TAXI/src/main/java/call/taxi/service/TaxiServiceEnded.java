cfb085a0d89f57d5d14a7ed7088a7992465c80f0
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:33:46 +0900

    Create TaxiServiceEnded.java

---
+++ TAXI/src/main/java/call/taxi/service/TaxiServiceEnded.java
@@ -0,0 +1,106 @@
+package call.taxi.service;
+
+public class TaxiServiceEnded extends AbstractEvent {
+
+    private Long callId;
+    private String memberId;
+    private String memberName;
+    private String startAddress;
+    private String endAddress;
+    private String taxiNumber;
+    private String taxiInfo;
+    private String phoneNumber;
+    private String friendPhoneNumber;
+    private String creditCardNumber;
+    private Long payAmt;
+    private String taxiStatus;
+
+    public TaxiServiceEnded(){
+        super();
+    }
+
+    public Long getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Long callId) {
+        this.callId = callId;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+    public String getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(String startAddress) {
+        this.startAddress = startAddress;
+    }
+    public String getEndAddress() {
+        return endAddress;
+    }
+
+    public void setEndAddress(String endAddress) {
+        this.endAddress = endAddress;
+    }
+    public String getTaxiNumber() {
+        return taxiNumber;
+    }
+
+    public void setTaxiNumber(String taxiNumber) {
+        this.taxiNumber = taxiNumber;
+    }
+    public String getTaxiInfo() {
+        return taxiInfo;
+    }
+
+    public void setTaxiInfo(String taxiInfo) {
+        this.taxiInfo = taxiInfo;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getFriendPhoneNumber() {
+        return friendPhoneNumber;
+    }
+
+    public void setFriendPhoneNumber(String friendPhoneNumber) {
+        this.friendPhoneNumber = friendPhoneNumber;
+    }
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+    public Long getPayAmt() {
+        return payAmt;
+    }
+
+    public void setPayAmt(Long payAmt) {
+        this.payAmt = payAmt;
+    }
+    public String getTaxiStatus() {
+        return taxiStatus;
+    }
+
+    public void setTaxiStatus(String taxiStatus) {
+        this.taxiStatus = taxiStatus;
+    }
+}
