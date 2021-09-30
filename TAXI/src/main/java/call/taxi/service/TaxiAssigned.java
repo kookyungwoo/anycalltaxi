eb3333651a960a8ed2e371f05dbb4ab24489ed15
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:33:24 +0900

    Create TaxiAssigned.java

---
+++ TAXI/src/main/java/call/taxi/service/TaxiAssigned.java
@@ -0,0 +1,34 @@
+package call.taxi.service;
+
+public class TaxiAssigned extends AbstractEvent {
+
+    private Integer callId;
+    private String taxiNumber;
+    private String taxiInfo;
+
+    public TaxiAssigned(){
+        super();
+    }
+
+    public Integer getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Integer callId) {
+        this.callId = callId;
+    }
+    public String getTaxiNumber() {
+        return taxiNumber;
+    }
+
+    public void setTaxiNumber(String taxiNumber) {
+        this.taxiNumber = taxiNumber;
+    }
+    public String getTaxiInfo() {
+        return taxiInfo;
+    }
+
+    public void setTaxiInfo(String taxiInfo) {
+        this.taxiInfo = taxiInfo;
+    }
+}
