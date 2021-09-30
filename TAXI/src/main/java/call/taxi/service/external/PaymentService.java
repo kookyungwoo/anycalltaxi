b4bf138702ea1243e4c18b1eba5198eb51ef703b
kkw9437 <kkw9437@gmail.com>
2021-09-30 23:53:48 +0900

    Update PaymentService.java

--- TAXI/src/main/java/call/taxi/service/external/PaymentService.java
+++ TAXI/src/main/java/call/taxi/service/external/PaymentService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@FeignClient(name="PAYMENT", url="http://PAYMENT:8080")
+@FeignClient(name="PAYMENT", url="http://localhost:8085", fallback = PaymentServiceFallback.class)
 public interface PaymentService {
     @RequestMapping(method= RequestMethod.GET, path="/payments")
     public void pay(@RequestBody Payment payment);
