33ea771c1af6dd4359cfc7ee8005542eb1022a6d
kkw9437 <kkw9437@gmail.com>
2021-09-30 13:28:33 +0900

    Create TaxiCallReceived.java

---
+++ MEMBERSHIP/src/main/java/call/taxi/service/TaxiCallReceived.java
@@ -0,0 +1,22 @@
+package call.taxi.service;
+
+public class TaxiCallReceived extends AbstractEvent {
+
+    private Long callId;
+    private String memberId;
+
+    public Long getCallId() {
+        return callId;
+    }
+
+    public void setCallId(Long callId) {
+        this.callId = callId;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+}
